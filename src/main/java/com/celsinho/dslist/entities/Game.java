9c1cf937af00ac5d73043dd8d535d5d09f1d1b42
Celso Bueno <cbtbueno@gmail.com>
2023-07-14 23:15:07 -0300

    Ajuste e Seed.

--- src/main/java/com/celsinho/dslist/entities/Game.java
+++ src/main/java/com/celsinho/dslist/entities/Game.java
@@ -2,7 +2,6 @@ package com.celsinho.dslist.entities;
 
 import java.util.Objects;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,26 +17,34 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    private String longDescripition;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl, String shortDescription, String longDescripition) {
+    public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl, String shortDescription, String longDescription) {
+    
         this.id = id;
-        this.title = title;
+        this.title = title;       
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescripition = longDescripition;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;        
+        this.shortDescription = shortDescription;        
+        this.longDescription = longDescription;
     }
 
     public Long getId() {
@@ -72,12 +79,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return this.plataform;
+    public String getPlatforms() {
+        return this.platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+    
+    public Double getScore() {
+        return this.score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -97,11 +112,11 @@ public class Game {
     }
 
     public String getLongDescripition() {
-        return this.longDescripition;
+        return this.longDescription;
     }
 
     public void setLongDescripition(String longDescripition) {
-        this.longDescripition = longDescripition;
+        this.longDescription = longDescripition;
     }
 
     @Override
